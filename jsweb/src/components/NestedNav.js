f0f747419cdf0f3cb5042b3128fd739106aad100
Lindanny9721 <32419597+Lindanny9721@users.noreply.github.com>
2025-03-15 20:45:38 -0400

    Added nestednav

--- jsweb/src/components/NestedNav.js
+++ jsweb/src/components/NestedNav.js
@@ -1,8 +1,12 @@
-import React from 'react'
-
+import React from 'react';
+import { Nav } from 'react-bootstrap';
 function NestedNav() {
   return (
-    <div>NestedNav</div>
+    <Nav defaultActiveKey="/home" className="flex-column second-navbar">
+    <Nav.Link href="/home">Active</Nav.Link>
+    <Nav.Link eventKey="link-1">Link</Nav.Link>
+    <Nav.Link eventKey="link-2">Link</Nav.Link>
+  </Nav>
   )
 }
 
